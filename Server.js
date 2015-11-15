e9cb9cba9bba2275aacef32218c48526a6a13db2
fredyj20135 <fredyj20135@gmail.com>
2015-11-15 11:40:23 +0800

    20151115 First commit

---
+++ Server.js
@@ -0,0 +1,113 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+
+var io = require('socket.io')(server);
+var fs = require('fs');
+
+/* Bing translator initialization */
+var mstranslator = require('mstranslator');
+var traanslateWizard = new mstranslator({
+	client_id: 'Super_ChatRoomWar_The1st',
+	client_secret: 'pcXvlEef4+Xjb9ryikhhvxgjeX3XosHtud1rmnOm8jQ='
+}, true);
+
+/* system log initialization */ 
+var userNumber = 0;
+var logStream;
+var logDir = './syslog/';
+var logName;
+
+/* express initialization */
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/public/Client.html');
+});
+
+server.listen(5092, '127.0.0.1', function() { console.log('listening on #: 5092'); });
+
+function getDateTime() {
+	var date = new Date();
+	var hour = date.getHours();			hour = (hour < 10 ? "0" : "") + hour;    
+    var min  = date.getMinutes();		min = (min < 10 ? "0" : "") + min;
+    var sec  = date.getSeconds();		sec = (sec < 10 ? "0" : "") + sec;
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;	month = (month < 10 ? "0" : "") + month;
+    var day  = date.getDate();			day = (day < 10 ? "0" : "") + day;
+
+    return year + month + day + '-' + hour + '-' + min + '-' + sec;
+}
+
+function initNewLog(){
+	if (userNumber == 0) {
+		logName = getDateTime() + '.csv';
+		logStream = fs.createWriteStream(logDir + logName);
+		logStream.write('User,Dialogue\n');
+	}
+}
+
+io.on('connection', function(socket) {
+	// the first event: if a user is linked to this server, open a dialogue box and input user ID
+	socket.on('addMe', function(username) {
+		socket.username = username;
+		socket.emit('serverSelfMsg', '[SERVER] You have connected');
+		socket.broadcast.emit('serverOthersMsg', '[SERVER] ' + username + ' is on deck');
+
+		// initNewLog();
+
+		userNumber = userNumber + 1;
+		console.log('usernumber = ' + userNumber);
+	});
+
+	// when someone is disconnect, print server information
+	socket.on('disconnect', function() {
+		socket.broadcast.emit('serverOthersMsg', '[SERVER] ' + socket.username + ' has left');
+		// logStream.end();
+		userNumber = userNumber - 1;
+	});
+
+	// when the socket with tag 'chat message' is received, send socket with tag 'chat' to all the user
+	socket.on('chat message', function(msg) {
+		var sysTime = getDateTime();
+		var content = {uid: socket.username, msg: msg, sysTime: sysTime};
+		io.sockets.emit('chat', content);
+		// console.log('[MSG][' + socket.username + ']' + msg + sysTime);
+
+		// var logMsg = socket.username + ',' + msg + '\n';
+		// logStream.write(logMsg);
+	});
+
+	// Pass translate result from server
+	socket.on('translate', function(input) {
+		var params = {
+			text: input.word,
+			from: input.fromLanguage,
+			to: input.toLanguage
+		};
+
+		traanslateWizard.translate(params, function(err, data) {
+			// for now, just return to one user!
+			// io.sockets.emit('is BINDED', {fromWord: input.word, toWord: data, timeStamp: input.timeStamp});
+			io.sockets.emit('is BINDED', {fromWord: input.word, toWord: data, timeStamp: input.timeStamp});
+			// console.log(input.word + data);
+		});
+	})
+	socket.on('blockMsg', function(block) {
+		socket.broadcast.emit('partnerMsgBlock', block);
+	});
+
+	socket.on('shareMsg', function(timeStamp) {
+		socket.broadcast.emit('partnerMsgShare', timeStamp);
+	});
+
+	socket.on('likeMsg', function(timeStamp) {
+		socket.broadcast.emit('partnerMsgLike', timeStamp);
+	});
+
+	socket.on('dislikeMsg', function(timeStamp) {
+		socket.broadcast.emit('partnerMsgDislike', timeStamp);
+	});
+
+});
+
---
+++ public/Client.js
@@ -0,0 +1,172 @@
+var socket = io.connect('http://localhost:5092');
+var username;
+var highlightWord;
+var partnerMsgBlock = true
+			
+/* Welcome and start */
+socket.on('connect', function() {
+	username = prompt('Who are you?');
+	socket.emit('addMe', username);
+	console.log(username);
+});
+
+/* distribute System Msg */ 
+socket.on('serverSelfMsg', function(msg) { /* server msg related to user */
+	$('#userMsgContainer').append($('<div>').text(msg).addClass('userMessage'));
+});
+
+socket.on('serverOthersMsg', function(msg) { /* server msg related to others */
+	$('#partnerMsgContainer').append($('<div>').text(msg).addClass('partnerMessage'));
+});
+
+socket.on('partnerMsgLike', function(timeStamp) { /* set like */
+	$('.userMessage:contains("' + timeStamp + '")').find('.likeBt').addClass('clicked');
+});
+
+socket.on('partnerMsgDislike', function(timeStamp) { /* Disliked */
+	$('.userMessage:contains("' + timeStamp + '")').find('.likeBt').removeClass('clicked');
+});
+
+/* Manipulate chat message, distribute user's and partner's messages
+ * msgPack : uid: USERNAME, msg: MESSAGE, sysTime: SYSTEM TIME */
+socket.on('chat', function(packet) {
+	var content = packet.msg.replace('\n', '<br>');
+	var uid = packet.uid;
+	var postTime = packet.sysTime;
+
+	var msgContainer;
+
+	var shareBt = $('<input>').addClass('shareBt').prop({type: 'button', value:''});
+	var likeBt = $('<input>').addClass('likeBt').prop({type: 'button', value:''});
+	var translateBtn = $('<input>').addClass('translateBt').prop({type: 'button', value: 'Translate'});
+	var timeStamp = $('<span>').addClass('timeStamp').hide().html(postTime);
+
+	content = $('<span>').addClass('msgContent').html(content + '<br>').append(timeStamp);
+
+	if (uid == username) {
+		msgContainer = $('#userMsgContainer');
+		content = content.append(shareBt).append(likeBt);
+		content = $('<div>').addClass('userMessage').append(content);
+	}else {
+		msgContainer = $('#partnerMsgContainer');
+		content = content.append(translateBtn).append(likeBt);
+		content = $('<div>').addClass('partnerMessage').append(content);
+
+		if (partnerMsgBlock == true) content.find('.msgContent').addClass('block');			
+	}
+	msgContainer.append(content);
+	msgContainer.scrollTop(msgContainer.prop("scrollHeight"));
+});
+
+socket.on('partnerMsgBlock', function(block){
+	partnerMsgBlock = block;
+	$('.partnerMessage').each(function(){
+		if ( !$(this).hasClass('share')) {
+			if (partnerMsgBlock == true) $(this).find('.msgContent').addClass('block');
+			else $(this).find('.msgContent').removeClass('block');
+		}
+	});
+});
+
+socket.on('partnerMsgShare', function(timeStamp) {
+	var partnerMsg = $('.partnerMessage:contains("' + timeStamp + '")');
+				
+	if (partnerMsg.hasClass('share') ){
+		partnerMsg.removeClass('share');
+		partnerMsg.find('.msgContent').addClass('block');
+	} else {
+		partnerMsg.addClass('share');
+		partnerMsg.find('.msgContent').removeClass('block');
+	}
+});
+
+socket.on('is BINDED', function(input) { /* Get translated data and add to message */
+	var replaceWord = input.fromWord + ' (' + input.toWord + ')';
+	var transMsg = $(".msgContent:contains('" + input.timeStamp + "')").closest('div');
+	var msgContainer = transMsg.parent();
+	var scroll = msgContainer.scrollTop()
+
+	transMsg.html(transMsg.html().replace(input.fromWord, '<span class = "highlight" >' + replaceWord + '</span>'));
+
+	msgContainer.scrollTop(scroll);
+});
+
+/* inputArea related functions. Concept by Alice Chang. Start*/
+function sendButtonHandler() {
+	socket.emit('chat message', $('#textInput').val());
+	$('#textInput').val('');
+}
+
+function clickControl(element) {
+	if (element.hasClass('clicked')) element.removeClass('clicked');
+   	else element.addClass('clicked');
+}
+
+/* Block message button*/
+$('#blockAll').click(function() { 
+	clickControl($(this));
+
+	if ($(this).hasClass('clicked')) socket.emit('blockMsg', true);
+	else socket.emit('blockMsg', false);
+});
+
+/* Like "partner's" message button */
+$('#container').on('click', '.partnerMessage input.likeBt', function() { 
+	clickControl($(this)); 
+
+	var partnerMsgLike = $(this).closest('.partnerMessage');
+	if ($(this).hasClass('clicked')) socket.emit('likeMsg', partnerMsgLike.find('.timeStamp').text());
+	else socket.emit('dislikeMsg', partnerMsgLike.find('.timeStamp').text());
+});
+
+/* Share "user's" message button*/
+$('#container').on('click', 'input.shareBt', function() { 
+	clickControl($(this)); 
+
+	var partnerMsgShare = $(this).closest('.userMessage')
+	if ($(this).hasClass('clicked')) partnerMsgShare.addClass('share');				
+	else partnerMsgShare.removeClass('share');
+				
+	socket.emit('shareMsg', partnerMsgShare.find('.timeStamp').text());
+});
+/* end */ 
+
+/* Catch user highlighted word (for translate)*/
+$('#container').on('mouseup', '.partnerMessage', function() {
+	highlightWord = getSelText().toString();
+
+	$('.partnerMessage').each( function(){ $(this).removeClass('selected'); });
+	if (highlightWord != '') $('.partnerMessage:contains("'+ highlightWord +'")').addClass('selected');
+});
+
+/* Send translate request */
+$('#container').on('click', 'input.translateBt', function() {
+	var partnerMsg = $(this).closest('.partnerMessage');
+	var transElement = {
+		word: highlightWord, 
+		timeStamp: partnerMsg.find('.timeStamp').text(),
+		fromLanguage: $('#oriLang').val(), 
+		toLanguage: $('#transLang').val() 
+	}
+
+	if (partnerMsg.hasClass('selected')){
+		socket.emit('translate', transElement);
+	}
+});
+
+/* Specify highlighted words */ 
+function getSelText() { 
+	var txt = '';
+	if (window.getSelection) {
+		txt = window.getSelection();
+	}else if (document.getSelection){
+		txt = document.getSelection();
+	}else if (document.selection) {
+		txt = document.selection.createRange().text;
+	}
+	return txt;
+}
+			
+$( document ).ready(function() {
+  	$('#sendButton').bind('click', sendButtonHandler);
+});
